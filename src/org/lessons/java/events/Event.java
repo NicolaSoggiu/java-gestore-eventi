20a9ff72fd6663d1171357c37bd80c243041ecf9
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-10-30 10:46:58 +0100

    milestone 1

--- src/org/lessons/java/events/Event.java
+++ src/org/lessons/java/events/Event.java
@@ -1,13 +1,16 @@
 package org.lessons.java.events;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Event {
+    // ATTRIBUTI
     private String title;
     private LocalDate date;
     private int totalSeats;
     private int reservedSeats;
 
+    // COSTRUTTORE
     public Event(String title, LocalDate date, int totalSeats) throws IllegalArgumentException {
         if (totalSeats <= 0) {
             throw new IllegalArgumentException("Something went wrong with the seats!");
@@ -23,6 +26,7 @@ public class Event {
         reservedSeats = 0;
     }
 
+    // GETTER E SETTER
     public String getTitle() {
         return title;
     }
@@ -45,5 +49,48 @@ public class Event {
 
     public int getReservedSeats() {
         return reservedSeats;
+    }
+
+    // METODO PER PRENOTARE I POSTI
+    public void bookSeats(int seats) throws IllegalArgumentException {
+        if (date.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("The event has already passed! You cannot reserve seats");
+        }
+
+        if (totalSeats >= reservedSeats) {
+            throw new IllegalArgumentException("Sold out!");
+        }
+
+        reservedSeats += seats;
+        totalSeats -= reservedSeats;
+    }
+
+    // METODO PER CANCELLARE UNA PRENOTAZIONE
+    public void cancelSeats(int seats) throws IllegalArgumentException {
+        if (date.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("The event has already passed! You cannot reserve seats");
+        }
+
+    if (reservedSeats <= 0) {
+        throw new IllegalArgumentException("There are no reserved seats!");
+    }
+
+        reservedSeats -= seats;
+        totalSeats += seats;
+    }
+
+    // METODO PER FORMATTARE LA DATA
+    public String formatData() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return date.format(formatter);
+    }
+    public String getDataFormattata() {
+        return formatData();
+    }
+
+    // TOSTRING
+    @Override
+    public String toString() {
+        return "Date: " + getDataFormattata() + "-" + "Title: " + title;
     }
 }
